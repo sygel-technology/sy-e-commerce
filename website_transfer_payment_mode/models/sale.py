b4efc4bcf029b5ab3256ddb54aa9b5ae1089a0c7
Manuel Florido <manuel.florido@sygel.es>
2024-07-10 16:36:43 +0200

    [T-5944][IMP] website_transfer_payment_mode:pre-commit auto fixes

--- website_transfer_payment_mode/models/sale.py
+++ website_transfer_payment_mode/models/sale.py
@@ -10,12 +10,20 @@ class SaleOrder(models.Model):
     # Pasar a la SO el tipo de pago seleccionado al enviar un correo
     def _send_order_confirmation_mail(self):
         if self.website_id or self.require_payment:
-            transaction_ids = self.env['payment.transaction'].search([
-                ('sale_order_ids', 'in', self.id),
-                ('state', 'in', ['pending', 'authorized', 'done'])
-            ], order="create_date desc")
-            if transaction_ids and transaction_ids[0] and \
-               transaction_ids[0].acquirer_id is not False and \
-               transaction_ids[0].acquirer_id.sale_payment_mode_id:
-                self.payment_mode_id = transaction_ids[0].acquirer_id.sale_payment_mode_id.id
+            transaction_ids = self.env["payment.transaction"].search(
+                [
+                    ("sale_order_ids", "in", self.id),
+                    ("state", "in", ["pending", "authorized", "done"]),
+                ],
+                order="create_date desc",
+            )
+            if (
+                transaction_ids
+                and transaction_ids[0]
+                and transaction_ids[0].acquirer_id is not False
+                and transaction_ids[0].acquirer_id.sale_payment_mode_id
+            ):
+                self.payment_mode_id = transaction_ids[
+                    0
+                ].acquirer_id.sale_payment_mode_id.id
         super(SaleOrder, self)._send_order_confirmation_mail()
